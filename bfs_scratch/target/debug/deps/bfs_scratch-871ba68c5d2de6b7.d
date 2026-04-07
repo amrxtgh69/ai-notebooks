fe67729d5c1eed26a00b65f373190881a7763d88
amrxtgh <ghimireamrit619@gmail.com>
2026-04-08 01:02:16 +0545

    add .gitignore

--- bfs_scratch/target/debug/deps/bfs_scratch-871ba68c5d2de6b7.d
+++
@@ -1,5 +0,0 @@
-/home/amrxtgh/ai-notebooks/bfs_scratch/target/debug/deps/bfs_scratch-871ba68c5d2de6b7.d: src/main.rs
-
-/home/amrxtgh/ai-notebooks/bfs_scratch/target/debug/deps/libbfs_scratch-871ba68c5d2de6b7.rmeta: src/main.rs
-
-src/main.rs:
--- bfs_scratch/target/debug/deps/bfs_scratch-ca992267f2457b3e.d
+++
@@ -1,9 +0,0 @@
-/home/amrxtgh/ai-notebooks/bfs_scratch/target/debug/deps/bfs_scratch-ca992267f2457b3e.d: src/main.rs Cargo.toml
-
-/home/amrxtgh/ai-notebooks/bfs_scratch/target/debug/deps/libbfs_scratch-ca992267f2457b3e.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
