5d830214c7158ba02516af5bc054b75cbaea1909
amrxtgh <ghimireamrit619@gmail.com>
2026-04-08 18:28:42 +0545

    start the fucking thing right now

--- dfs_scratch/src/main.rs
+++ dfs_scratch/src/main.rs
@@ -1,3 +1,28 @@
+use std::collections::HashMap;
+
+enum Color {
+    White,
+    Gray,
+    Black,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut graph: HashMap<usize, Vec<usize>> = HashMap::new();
+    graph.insert(0, vec![1, 2]);
+    graph.insert(1, vec![2]);
+    graph.insert(2, vec![0, 3]);
+    graph.insert(3, vec![3]);
+
+    let mut keys: Vec<_> = graph.keys().collect();
+    keys.sort();
+
+    for node in keys {
+        let neighbors = &graph[node];
+        let neighbors_str = neighbors
+            .iter()
+            .map(|n| n.to_string())
+            .collect::<Vec<_>>()
+            .join(", ");
+        println!("{} -> {}", node, neighbors_str);
+    }
 }
