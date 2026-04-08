4b6e24a97d4d11362f373ef5377c259e4b6c3c8c
amrxtgh <ghimireamrit619@gmail.com>
2026-04-08 23:12:05 +0545

    add discovery time and the finishing time

--- dfs_scratch/src/main.rs
+++ dfs_scratch/src/main.rs
@@ -5,26 +5,41 @@ enum Color {
     Black,
 }
 
-fn dfs(start: usize, graph: &Vec<Vec<usize>>, color: &mut Vec<Color>) {
+fn dfs(
+    start: usize,
+    graph: &Vec<Vec<usize>>,
+    color: &mut Vec<Color>,
+    time: &mut i32,
+    d: &mut Vec<i32>,
+    r: &mut Vec<i32>,
+) {
+    *time += 1;
+    d[start] = *time;
     color[start] = Color::Gray;
     for &neighbour_node in &graph[start].clone() {
         if color[neighbour_node] == Color::White {
-            dfs(neighbour_node, graph, color);
+            dfs(neighbour_node, graph, color, time, d, r);
         }
     }
     color[start] = Color::Black;
+    *time += 1;
+    r[start] = *time;
 }
 
 fn main() {
     let graph: Vec<Vec<usize>> = vec![vec![1, 2], vec![2], vec![3], vec![1]];
     let n = graph.len();
-    let start = 0;
     let mut color = vec![Color::White; n];
+    let mut d = vec![0; n];
+    let mut r = vec![0; n];
+    let mut time = 0;
 
-    dfs(start, &graph, &mut color);
+    let start = 0;
 
-    println!("Final colors:");
+    dfs(start, &graph, &mut color, &mut time, &mut d, &mut r);
+
+    println!("Node -> Discovery Time / Finishing Time:");
     for i in 0..n {
-        println!("Node {} -> {:?}", i, color[i]);
+        println!("Node {} -> {} / {}", i, d[i], r[i]);
     }
 }
