623df3210f21fb21c10d9af84a2c1de97300962b
amrxtgh <ghimireamrit619@gmail.com>
2026-04-12 00:45:10 +0545

    init

--- astar/src/main.rs
+++ astar/src/main.rs
@@ -1,4 +1,8 @@
 // This is the implementation of the A Star algorithm which is one informed search strategy too.
 
+use std::collections::HashMap;
+
+type Graph<V, E> = HashMap<V, Vec<(V, E)>>;
+
 fn main() {
 }
