fa376d111581bbfba84b87dc475a0d4847b592b8
amrxtgh <ghimireamrit619@gmail.com>
2026-06-18 08:15:06 +0545

    how perceptron works

---
+++ perceptron/src/lib.rs
@@ -0,0 +1,66 @@
+pub struct Perceptron {
+    weight: Vec<f64>,
+    bias: f64,
+    lr: f64,
+}
+
+impl Perceptron {
+    pub fn new(n_features: usize, lr: f64) -> Self {
+        Self {
+            weight: vec![0.0; n_features],
+            bias: 0.0,
+            lr,
+        }
+    }
+
+    pub fn predict(&self, inputs: &[f64]) -> i32 {
+        let sum: f64 = inputs
+            .iter()
+            .zip(&self.weight)
+            .map(|(x, w)| x * w)
+            .sum::<f64>()
+            + self.bias;
+        if sum >= 0.0 {
+            1
+        } else {
+            -1
+        }
+    }
+
+    pub fn train(&mut self, inputs: &[f64], target: i32) {
+        let pred = self.predict(inputs);
+        let error = target - pred;
+        if error != 0 {
+            for (w, x) in self.weight.iter_mut().zip(inputs) {
+                *w += self.lr * error as f64 * x;
+            }
+            self.bias += self.lr * error as f64;
+        }
+    }
+
+    pub fn fit(&mut self, data: &[(&[f64], i32)], epochs: usize) {
+        for _ in 0..epochs {
+            for (inputs, target) in data {
+                self.train(inputs, *target);
+            }
+        }
+    }
+}
+
+pub fn and_data() -> Vec<(Vec<f64>, i32)> {
+    vec![
+        (vec![0.0, 0.0], -1),
+        (vec![0.0, 1.0], -1),
+        (vec![1.0, 0.0], -1),
+        (vec![1.0, 1.0], 1),
+    ]
+}
+
+pub fn or_data() -> Vec<(Vec<f64>, i32)> {
+    vec![
+        (vec![0.0, 0.0], -1),
+        (vec![0.0, 1.0], 1),
+        (vec![1.0, 0.0], 1),
+        (vec![1.0, 1.0], 1),
+    ]
+}
---
+++ perceptron/src/main.rs
@@ -0,0 +1,39 @@
+use perceptron::*;
+
+fn main() {
+    println!("AND gate:");
+    let mut p = Perceptron::new(2, 0.1);
+    let and_data = and_data();
+    let and_ref: Vec<(&[f64], i32)> = and_data
+        .iter()
+        .map(|(x, y)| (x.as_slice(), *y))
+        .collect();
+    p.fit(&and_ref, 10);
+
+    for (inputs, target) in &and_data {
+        println!(
+            "  {:?} -> {} (expected {})",
+            inputs,
+            p.predict(inputs),
+            target
+        );
+    }
+
+    println!("\nOR gate:");
+    let mut p = Perceptron::new(2, 0.1);
+    let or_data = or_data();
+    let or_ref: Vec<(&[f64], i32)> = or_data
+        .iter()
+        .map(|(x, y)| (x.as_slice(), *y))
+        .collect();
+    p.fit(&or_ref, 10);
+
+    for (inputs, target) in &or_data {
+        println!(
+            "  {:?} -> {} (expected {})",
+            inputs,
+            p.predict(inputs),
+            target
+        );
+    }
+}
